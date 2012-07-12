b8a21b01638f0c183ad93e037b8847df7a7691cb
David Precious <davidp@preshweb.co.uk>
2012-07-12 15:24:07 +0100

    Make this compatible with Redmine 2.x.

--- init.rb
+++ init.rb
@@ -1,7 +1,6 @@
 require 'redmine'
-require 'dispatcher'
 
-Dispatcher.to_prepare do
+Rails.configuration.to_prepare do
   require_dependency 'journal_hook'
 end
 
@@ -11,5 +10,6 @@ Redmine::Plugin.register :redmine_mention_plugin do
   description 'Add user to watcher list after mentioning him/her (e.g. @john) in issue note'
   version '0.1'
   url 'http://github.com/steply/redmine-mention-plugin'
+  requires_redmine :version_or_higher => '2.0.0'
   author_url 'http://steply.com'
 end
