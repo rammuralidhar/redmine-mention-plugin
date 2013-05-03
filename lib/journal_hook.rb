dd7304d5e108cfe155321b489f20b018aaa3f93b
Diego Antunes <diegotdai@gmail.com>
2013-05-04 11:46:10 +1200

    Only matching strings that start with @ to avoid emails

--- lib/journal_hook.rb
+++ lib/journal_hook.rb
@@ -8,7 +8,7 @@ module Mention
         if journal.journalized.is_a?(Issue) && journal.notes.present?
           issue = journal.journalized
           # TODO Should ignore email
-          mentioned_users = journal.notes.scan(/\:\w+/)
+          mentioned_users = journal.notes.scan(/^\@\w+/)
           mentioned_users.each do |mentioned_user|
             username = mentioned_user[1..-1] # Remove the heading ':'
             if user = User.find_by_login(username)
