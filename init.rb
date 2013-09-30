af7bdb5b11b7ccd397b4e798764ad6b711a17c16
Diego Antunes <diegotdai@gmail.com>
2013-10-01 09:44:37 +1300

    Match usernames with a separator, such as @user.name

--- init.rb
+++ init.rb
@@ -12,9 +12,9 @@ end
 Redmine::Plugin.register :redmine_mention_plugin do
   name 'Redmine Mention Plugin'
   author 'Steply'
-  description 'Add user to watcher list after mentioning him/her (e.g. @john) in issue note'
-  version '0.1'
-  url 'http://github.com/steply/redmine-mention-plugin'
+  description 'Add user to watcher list after mentioning him/her (e.g. @john or @jhon.who) in issue note'
+  version '0.2'
+  url 'http://github.com/diegotdai/redmine-mention-plugin'
   requires_redmine :version_or_higher => '2.0.0'
   author_url 'http://steply.com'
 end
