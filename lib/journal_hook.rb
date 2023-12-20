79e700e814c395dd6ea2ce67d1cd127fa7e0342e
ram m <ram.muralidhar@ext.smile.eu>
2023-12-20 11:56:34 +0530

    #1266403 - Added mention users mail

--- lib/journal_hook.rb
+++ lib/journal_hook.rb
@@ -8,11 +8,20 @@ require_dependency 'watcher'
         if journal.journalized.is_a?(Issue) && journal.notes.present?
           issue = journal.journalized
           # TODO Should ignore email
+          users = self.notified_users | self.notified_watchers
+          users.select! do |user|
+            self.notes? || self.visible_details(user).any?
+          end
+          user_login_ids = users.map{|u| u.login}
+          
           mentioned_users = journal.notes.scan(/@[\w.]+/)
           mentioned_users.each do |mentioned_user|
             username = mentioned_user[1..-1] # Remove the heading ':'
             if user = User.find_by_login(username)
               Watcher.create(:watchable => issue, :user => user)
+              if (!user_login_ids.include?(username))
+                MentionMailer.notify_mentioning(issue, self, user).deliver
+              end
             end
           end
         end
