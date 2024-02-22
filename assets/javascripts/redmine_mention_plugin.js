b1af91c10785ec6a45734b2f7aff48259610dae9
ram m <ram.muralidhar@ext.smile.eu>
2024-02-22 19:05:36 +0530

    #1266403 - Mention user.

--- assets/javascripts/redmine_mention_plugin.js
+++ assets/javascripts/redmine_mention_plugin.js
@@ -10,16 +10,39 @@ $(document).ready(function() {
         element.append(template);
     };
     
-    $.getJSON('/autocomplete/mention/user', function(data) {
-        $.each(data, function(user) {
-          user = {
-              val: this.login,
-              meta: this.firstname + " " + this.lastname
-          };
-          values.push(user);
+    var projectId = $("#issue_project_id").val();
+    if (projectId) {
+        var watcherText = $("#watchers h3").text();
+
+        $("body").parent().bind("DOMSubtreeModified", function() {
+            var newProjectId = $("#issue_project_id").val();
+            var newwatcherText = $("#watchers h3").text();
+            if ((newProjectId && newProjectId != projectId)  || (newwatcherText && newwatcherText != watcherText)) {
+                projectId = newProjectId;
+                watcherText = newwatcherText;
+                ajaxMentionUsers();
+            }
         });
-    }).done(function() {
-    	$(window).trigger('feedLoaded', [{data: values}]);
-        $("#issue_notes, #issue_description").sew({values: values, elementFactory: elementFactory, token: '@'});
-    });
+        ajaxMentionUsers();
+        function ajaxMentionUsers() {
+            var issue_id = '';
+            var pathname = window.location.pathname;
+            if (pathname.startsWith("/issues/")) {
+                issue_id = "&issue_id=" + pathname.substring(pathname.lastIndexOf('/') + 1);
+            }
+            $.getJSON('/autocomplete/mention/user?project_id=' + projectId + issue_id, function(data) {
+                values = [];
+                $.each(data, function(user) {
+                user = {
+                    val: this.login,
+                    meta: this.firstname + " " + this.lastname
+                };
+                values.push(user);
+                });
+            }).done(function() {
+                $(window).trigger('feedLoaded', [{data: values}]);
+                $("#issue_notes, #issue_description").sew({values: values, elementFactory: elementFactory, token: '@'});
+            });
+        }
+    }
 });
--- lib/auto_completes_controller_patch.rb
+++ lib/auto_completes_controller_patch.rb
@@ -8,10 +8,24 @@ module AutoCompletesControllerPatch
     # Wraps the association to get the Deliverable subject.  Needed for the 
     # Query and filtering
     def autocomplete_mention_user
-      @users = User.logged.select("id, login, firstname, lastname")
-      
+      if @project
+        @users = @project.users.to_a.delete_if{|u| (u.type != 'User' || u.mail.empty?)}
+      else
+      	@users = User.logged.select("id, login, firstname, lastname") 
+      end
+
+      if params[:issue_id].present?
+        watcher_user_ids = Issue.find(params[:issue_id]).watcher_user_ids
+        
+        watcher_user_ids.each do |watcher_user_id|
+          unless @users.any? {|user| user.id == watcher_user_id}
+            @users << User.find(watcher_user_id);
+          end
+        end
+      end
       render :json => @users
     end
+
   end
 end
 
